2be049d179e0f5c980cd2db41273a620831167cb
LdChambers <l.d.chambers@tcu.edu>
2025-03-10 06:59:34 -0500

    Vue-named-slots

---
+++ src/BaseLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <header>
+            <!-- Header Content here -->
+             <slot name="header"></slot>
+        </header>
+        <main>
+            <!-- Main content here -->
+             <slot name="main"></slot>
+        </main>
+        <footer>
+            <!-- Footer content here -->
+             <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
