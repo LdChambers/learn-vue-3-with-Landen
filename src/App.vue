72baeee280ea485a2bdd997c9d9a51e6b427ea97
LdChambers <l.d.chambers@tcu.edu>
2025-03-10 06:44:35 -0500

    Vue-slot

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    Hello, Vue!
-  </div>
+  <AlertBox>Invalid Username or password. PLease try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresg the page and try again.</h1>
+  </AlertBox>
+  
 </template>
 
 <script setup>
-
+import AlertBox from './AlertBox.vue';
 </script>
 
 <style lang="scss" scoped>
