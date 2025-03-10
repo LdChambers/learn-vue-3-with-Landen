0e28c3cd17bbef593b216f80077a8d6bc63a4be2
LdChambers <l.d.chambers@tcu.edu>
2025-03-10 06:28:45 -0500

    Pass data to descendant with provide and inject.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    Hello, Vue!
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px;">
+    <ComponentA />
   </div>
 </template>
 
 <script setup>
+import { ref, provide, onUpdated, readonly } from 'vue'
+import ComponentA from './ComponentA.vue';
 
+let message = ref('Hello, Provide/inject!')
+
+function updateMessage() {
+  message.value = 'Hello, Provide/inject! Updated!'
+
+}
+
+let count = ref(0)
+provide('read-only-count', readonly(count))
+
+provide('msg', {message, updateMessage})
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
