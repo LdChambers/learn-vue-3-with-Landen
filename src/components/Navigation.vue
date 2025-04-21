e10e72febe05ada9d0cea9dea83f91e2f4eb4611
LdChambers <l.d.chambers@tcu.edu>
2025-04-21 17:35:31 -0500

    Nested routes and dynamic route matching.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
        <router-link :to="{name: 'home'}">Home</router-link>
-        <router-link :to="{name: 'blogposts'}">Blog Posts</router-link>
+        <router-link :to="{name: 'blogPosts'}">Blog Posts</router-link>
         <router-link :to="{name: 'about'}">About</router-link>
         
     </nav>
--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import BlogPosts from '@/views/BlogPosts.vue'
+import BlogPost from '@/views/BlogPost.vue'
+import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
     routes: [ 
         {path: '/', name:'home', component: Home},
-        {path: '/BlogPosts',name:'blogposts', component: BlogPosts},
+        {path: '/blogPosts',name:'blogPosts', component: BlogPosts,
+          redirect: {name:"blogPostsGreeting"}, 
+          children: [
+            {path: '',name: 'blogPostsGreeting', component: BlogPostsGreeting},
+          {path: 'blogPosts/:id(\\d+)', name: 'blogPost', component: BlogPost},
+        ]},
         {path: '/About',name:'about', component: About},
+        {path: '/:catchAll(.*)*', name: 'notFound', component: NotFound},
     ]
         
 })
